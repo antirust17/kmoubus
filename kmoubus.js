752ed6afd0bb20b1adcc941b00d50e034872f605
Jeon WooYoung <antirust17@gmail.com>
2016-07-28 00:16:36 +0900

    진짜 마지막

--- kmoubus.js
+++ kmoubus.js
@@ -1,12 +1,12 @@
 window.onload=function funcKMOUBUS(){
-	var now = new Date();
-	var nowYear = now.getFullYear();
-	var nowMonth = now.getMonth() + 1;
-	var nowDay = now.getDate();
-	var nowHour = now.getHours();
-	var nowMin = now.getMinutes();
-	var nowSec = now.getSeconds();
-	var nowWeek = now.getDay();
+	now = new Date();
+	nowYear = now.getFullYear();
+	nowMonth = now.getMonth() + 1;
+	nowDay = now.getDate();
+	nowHour = now.getHours();
+	nowMin = now.getMinutes();
+	nowSec = now.getSeconds();
+	nowWeek = now.getDay();
 	nowWeekText = ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"];
 
 	//date varable의 할당 문제. http://stackoverflow.com/questions/6609574/javascript-date-variable-assignment 참고
@@ -14,41 +14,33 @@ window.onload=function funcKMOUBUS(){
 	now.setMonth(arr1[1].getMonth());
 	now.setDate(arr1[1].getDate());
 	
-	var nowVacation = true; //방학일경우 true, 학기중이면 false
+	nowVacation = true; //방학일경우 true, 학기중이면 false
 
 	//순환버스용 요일, 방학 여부 판단
-	if (nowWeek == 0 || nowWeek == 6) {
+	if (nowWeek == 0 || nowWeek == 6) 
 		arrShuttle = arr1;
-	}
-	else if (nowVacation == true) {
+	else if (nowVacation == true) 
 		arrShuttle = arr2;
-	}
-	else {
+	else 
 		arrShuttle = arr3;
-	}
 
 	//순환버스 다음 출발시간 찾기
-	var shuttleLoop = 1;
-	while (now.getTime() >= arrShuttle[shuttleLoop].getTime()) {
+	shuttleLoop = 1;
+	while (now.getTime() >= arrShuttle[shuttleLoop].getTime()) 
 		shuttleLoop++;
-	}
 
 	//시내버스용 요일 판단
-	if (nowWeek == 0 ) {
+	if (nowWeek == 0 )
 		arr190 = arr4;
-	}
-	else if (nowWeek == 6) {
+	else if (nowWeek == 6)
 		arr190 = arr5;
-	}
-	else {
+	else 
 		arr190 = arr6;
-	}
 
 	//190번 시내버스 다음 출발시간 찾기 
-	var cityLoop = 1;
+	cityLoop = 1;
 	while (now.getTime() >= arr190[cityLoop].getTime()) 
 		cityLoop++;
-	
 
 	//현재시간 출력
 	for (i=0;i<document.getElementsByClassName("nowTime").length;i++)
