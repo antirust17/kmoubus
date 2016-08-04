bee15b196857e6a8f029e4ea4d1e36bc1a5ccf6d
Jeon WooYoung <antirust17@gmail.com>
2016-08-04 12:16:01 +0900

    jquery를 사용하지 않고 자바스크립트로 div에 html코드를 로드하게 함

---
+++ js/kmoubus-archisum-mobile.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+	kmoubusMain();
+	document.getElementById("kmoubus-mobile-header").innerHTML = '│셔틀 <strong><span class="kmoubus-nextShuttleRemaining"></span>분</strong> 전│190 <strong><span class=kmoubus-next190Remaining></span>분</strong> 전│<a href="http://archisum.net/me_911"><img src="https://twemoji.maxcdn.com/36x36/1f4e2.png" width=13 height=13> 셔틀버스 불편신고</a>│'
+}
