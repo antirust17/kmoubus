3144ce1191705f8f6b91744bf84d3635de39038c
Jeon WooYoung <antirust17@gmail.com>
2016-08-05 17:20:26 +0900

    블로그용 js

---
+++ js/kmoubus-antirustblog.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+	kmoubusMain();
+	document.getElementById("kmoubus-antirustblog-header").innerHTML = '<span class="kmoubus-nowTime"></span> | 셔틀 <span class="kmoubus-nextShuttleRemaining"></span>분 남음 | 190 <span class=kmoubus-next190Remaining></span>분 남음'
+}
