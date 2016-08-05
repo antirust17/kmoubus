575c3d765a13fa2c363e78001178bfcb02f0cb19
Jeon WooYoung <antirust17@gmail.com>
2016-08-05 17:49:53 +0900

    antirustblog에는 html로드하지 않고 그냥 코드를 삽입하였음

--- js/kmoubus-antirustblog.js
+++ js/kmoubus-antirustblog.js
@@ -1,4 +1,3 @@
 window.onload = function() {
 	kmoubusMain();
-	document.getElementById("kmoubus-antirustblog-header").innerHTML = '<span class="kmoubus-nowTime"></span> | 셔틀 <span class="kmoubus-nextShuttleRemaining"></span>분 남음 | 190 <span class=kmoubus-next190Remaining></span>분 남음'
 }
