6cf3f5522004b56879fd5cbe65d6ac823546a8a8
unknown <>
2016-08-26 13:28:24 +0900

    버스정보 자세히보기 추가 등

--- js/kmoubus-archisum-mobile.js
+++ js/kmoubus-archisum-mobile.js
@@ -1,4 +1,4 @@
 window.onload = function() {
 	kmoubusMain();
-	document.getElementById("kmoubus-mobile-header").innerHTML = '│셔틀 <strong><span class="kmoubus-nextShuttleRemaining"></span>분</strong> 전│190 <strong><span class=kmoubus-next190Remaining></span>분</strong> 전│<a href="http://archisum.net/me_911"><img src="https://twemoji.maxcdn.com/36x36/1f4e2.png" width=13 height=13> 셔틀버스 불편신고</a>│'
+	document.getElementById("kmoubus-mobile-header").innerHTML = '│셔틀 <strong><span class="kmoubus-nextShuttleRemaining"></span>분</strong> 전│190 <strong><span class=kmoubus-next190Remaining></span>분</strong> 전│<a href="http://antirust.tk/kmoubus"><img src="kmoubus/img/bus.png" width=13 height=13></a>│<a href="http://archisum.net/index.php?mid=co_free&category=267"><img src="kmoubus/img/singo.png" width=13 height=13></a>'
 }
