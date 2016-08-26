e8c89833d0a9d2ea4a6b92ea769335f1ae399617
unknown <>
2016-08-26 13:36:50 +0900

    그림이 안 보일 때 출력될 문구 추가, 세로 정렬 조정

--- js/kmoubus-archisum-mobile.js
+++ js/kmoubus-archisum-mobile.js
@@ -1,4 +1,4 @@
 window.onload = function() {
 	kmoubusMain();
-	document.getElementById("kmoubus-mobile-header").innerHTML = '│셔틀 <strong><span class="kmoubus-nextShuttleRemaining"></span>분</strong> 전│190 <strong><span class=kmoubus-next190Remaining></span>분</strong> 전│<a href="http://antirust.tk/kmoubus"><img src="kmoubus/img/bus.png" width=13 height=13></a>│<a href="http://archisum.net/index.php?mid=co_free&category=267"><img src="kmoubus/img/singo.png" width=13 height=13></a>'
+	document.getElementById("kmoubus-mobile-header").innerHTML = '│셔틀 <strong><span class="kmoubus-nextShuttleRemaining"></span>분</strong> 전│190 <strong><span class=kmoubus-next190Remaining></span>분</strong> 전│<a href="http://antirust.tk/kmoubus"><img src="kmoubus/img/bus.png" width=13 height=13 alt="버스정보 자세히보기" style="vertical-align: -30%"></a>│<a href="http://archisum.net/index.php?mid=co_free&category=267"><img src="kmoubus/img/singo.png" width=13 height=13 alt="아치신문고" style="vertical-align: -30%"></a>'
 }
